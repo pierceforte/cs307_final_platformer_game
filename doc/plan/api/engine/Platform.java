41de7812b5bba09bf8a7232210d76c6fb68740c3
Pierce Forte <phf7@duke.edu>
2020-04-02 23:00:38 -0400

    Added comments to APIs

--- doc/plan/api/engine/Platform.java
+++ doc/plan/api/engine/Platform.java
@@ -3,7 +3,14 @@
  */
 public interface Platform {
 
+    /**
+     * Define how platform moves
+     */
     void move();
 
+    /**
+     * Define how entities are affected when on top of platform
+     * @param entity Entity that is on top of platform
+     */
     void handleEntityInteraction(GameObject entity);
 }
