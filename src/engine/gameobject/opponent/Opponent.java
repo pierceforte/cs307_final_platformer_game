01b8722d99cbf151f6d939f63a5a1b61dbff7cb7
Pierce Forte <phf7@duke.edu>
2020-04-09 18:34:45 -0400

    Added test class and wrote tests that expect to fail

--- src/engine/gameobject/opponent/Opponent.java
+++ src/engine/gameobject/opponent/Opponent.java
@@ -1,11 +1,10 @@
+package engine.gameobject.opponent;
 
 /**
  * This interface defines how a unique opponent will operate
  */
 public interface Opponent {
 
-    void assignLogic() {
-        // can I jump over things? can I swim? can I use certain objects?
-    }
+    void assignLogic();
 
 }
