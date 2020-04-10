2bba3247833aa7bc8a88993082a34703fe3a5c7a
Benjamin Burnett <bcb44@duke.edu>
2020-04-09 22:15:46 -0400

    Forgot to move back data package

---
+++ src/data/Firebase.java
@@ -0,0 +1,33 @@
+package data;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Firebase {
+
+    Firestore db;
+
+    public Firebase() throws IOException {
+        // Use a service account
+        InputStream serviceAccount = new FileInputStream("src/data/ServiceAccountKey.json");
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(credentials)
+                .build();
+        FirebaseApp.initializeApp(options);
+        db = FirestoreClient.getFirestore();
+    }
+
+    public Firestore getDatabase() {
+        return db;
+    }
+}
