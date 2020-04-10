d41b87a0205cd27911b3bdc7dcfe2cb36c525185
Nicole Lindbergh <nl152@duke.edu>
2020-04-09 22:02:30 -0400

    merging

--- src/data/User.java
+++ src/data/User.java
@@ -9,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.text.ParseException;
 import java.util.*;
 
 public class User {
