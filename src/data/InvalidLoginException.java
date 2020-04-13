e279a9e93c7f37cd9b00a12ef4998192dd93d3c0
Benjamin Burnett <bcb44@duke.edu>
2020-04-13 15:27:54 -0400

    push for professor duvall

--- src/data/InvalidLoginException.java
+++
@@ -1,8 +0,0 @@
-package data;
-
-public class InvalidLoginException extends Exception {
-    private static final String message = "Username (%s) and password (%s) do not match";
-    public InvalidLoginException(String userID, String password) {
-        super(String.format(message, userID, password));
-    }
-}
--- src/input/KeyInput.java
+++ src/input/KeyInput.java
@@ -1,4 +1,4 @@
-package data;
+package data.input;
 
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
