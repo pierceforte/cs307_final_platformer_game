88cba89bbed5fbac5464f2c8e4b07b8c8b0a3c7d
Benjamin Burnett <bcb44@duke.edu>
2020-04-16 22:15:33 -0400

    Checking out jerrys branch

--- src/data/Tester.java
+++ src/data/Tester.java
@@ -1,17 +1,32 @@
 package data;
 
-import data.levels.SaveLevel;
 import data.user.InvalidLoginException;
 import data.user.ReadSaveException;
 import engine.gameobject.opponent.Mongoose;
-
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
+import java.lang.reflect.Type;
+
 
 public class Tester {
     public static void main(String[] args) throws InvalidLoginException, ReadSaveException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        SaveLevel level = new SaveLevel();
-        level.save(Arrays.asList(new Mongoose(1.0,2.0,1.0)));
+        Mongoose mini = new Mongoose(1,1,1);
+        Class wee = mini.getClass();
+        Constructor ctor = wee.getConstructor();
+        System.out.println(ctor.toString());
+        Type[] me = ctor.getParameterTypes();
+        for (Type param : me) {
+            System.out.println(param);
+            System.out.println(param.getClass());
+        }
+        Double eep = 9.0;
+        Object we = (Object) eep;
+        System.out.println("0" + we.getClass().toString());
+        String className = "java.lang.Double";
+        Class e = Class.forName(className);
+        Mongoose v = Mongoose.class.getConstructor(e, e, e).newInstance(2.2, 2.2, 2.2);
+        v.getImgPath();
+        //System.out.println(Arrays.toString());
         //User oops = new User("bcb44", "benburnett");
         //NewUser user = new NewUser("pierce", "password", "home.img");
 //        Firebase database = new Firebase();
--- src/data/levels/SaveLevel.java
+++ src/data/levels/SaveLevel.java
@@ -12,9 +12,8 @@ import org.json.simple.parser.JSONParser;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Type;
+import java.util.*;
 
 public class SaveLevel {
     JSONObject levels;
@@ -31,7 +30,7 @@ public class SaveLevel {
         }
     }
 
-    public void save(List<GameObject> list) {
+    public void save(List<GameObject> list) throws ReadSaveException {
         if (!levels.containsKey("temp")) levels.put("temp", new JSONObject());
         JSONObject temp = (JSONObject) levels.get("temp");
         temp.clear();
@@ -40,18 +39,57 @@ public class SaveLevel {
         }
     }
 
-    private void addObj(GameObject object, JSONObject temp) {
+    private void addObj(GameObject object, JSONObject temp) throws ReadSaveException {
         String objClass = object.getClass().toString();
         System.out.println(objClass);
-        if (!levels.containsKey(objClass)) levels.put(objClass, new JSONArray());
-        JSONArray objList = (JSONArray) levels.get(objClass);
+        if (!levels.containsKey(objClass)) temp.put(objClass, new JSONArray());
+        JSONArray objList = (JSONArray) temp.get(objClass);
         JSONArray instanceList = new JSONArray();
         for (Object param : object.getParameters()) {
-            JSONObject paramDict = new JSONObject();
-            paramDict.put(object.getClass().toString(), object.toString());
-            instanceList.add(paramDict);
+            JSONArray paramList = new JSONArray();
+            paramList.addAll(Arrays.asList(param.getClass().toString(), param.toString()));
+            instanceList.add(paramList);
         }
         objList.add(instanceList);
+        write();
+    }
+
+    public List<GameObject> getTempSave() throws ReadSaveException, ClassNotFoundException {
+        return getSavedLevel("temp");
+    }
+
+    public List<GameObject> getSavedLevel(Object level) throws ReadSaveException, ClassNotFoundException {
+        List<GameObject> levelObjects = new ArrayList<>();
+        String target;
+        if (!(level instanceof java.lang.String)) target = Integer.toString((Integer)level);
+        else target = (String) level;
+        if (!levels.containsKey(target)) throw new ReadSaveException("read", fileLoc);
+        List<GameObject> saved = new ArrayList<>();
+        JSONObject temp = (JSONObject) levels.get("temp");
+        for (Object key : temp.keySet()) {
+            String className = (String) key;
+            Class objClass = Class.forName(className);
+            JSONArray listOfType = (JSONArray) temp.get(className);
+            for (Object obj : listOfType) {
+                levelObjects.add(makeObject(objClass, (JSONArray) obj));
+            }
+
+        }
+        return Arrays.asList(new Mongoose(1.0,2.0,3.0));
+    }
+
+    private GameObject makeObject(Class objClass, JSONArray parameters) throws ClassNotFoundException {
+        List<Type> types = new ArrayList<>();
+        List<Object> types = new ArrayList<>();
+        for (Object paramObj : parameters) {
+            JSONArray param = (JSONArray) paramObj;
+            Class t = Class.forName((String) param.get(0));
+            Object par = t.getDeclaredConstructor().newInstance()
+            types.add((param.get(1));
+        }
+        // call constructor whose parameter matches the given class type
+        GameObject object = objClass.getDeclaredConstructor(String.class).newInstance("Test");
+        System.out.println("Printing: " + o);
     }
 
     /**
