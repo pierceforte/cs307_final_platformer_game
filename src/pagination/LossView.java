f39b3d35a21388d7b36becf3aa3e65fe2a7275f2
Nicole Lindbergh <nl152@duke.edu>
2020-04-24 23:05:44 -0400

    added resource bundle for win loss screen

--- src/pagination/LossView.java
+++ src/pagination/LossView.java
@@ -3,8 +3,12 @@ package pagination;
 import engine.leveldirectory.gamesequence.GameSeqLevelController;
 import javafx.scene.text.Text;
 
+import java.util.ResourceBundle;
+
 public class LossView extends ResultStage {
 
+
+    private final ResourceBundle myResource = ResourceBundle.getBundle("text.WinLoss");
 
     public LossView(PageController PC,  GameSeqLevelController Controller) {
         super(PC);
@@ -12,7 +16,7 @@ public class LossView extends ResultStage {
     }
 
     private void standardDisplayText() {
-        Text t = new Text("You've lost :(");
+        Text t = new Text(myResource.getString("StandardLoss"));
         t.setId("Welcome");
         this.getChildren().add(t);
     }
--- src/pagination/WinView.java
+++ src/pagination/WinView.java
@@ -5,12 +5,15 @@ import engine.leveldirectory.gamesequence.GameSeqLevelController;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
+import java.util.ResourceBundle;
+
 public class WinView extends ResultStage {
 
     private PageController myPC;
     private GameSeqLevelController controller;
 
     private final static int PrizeCoins = 200;
+    private final ResourceBundle myResource = ResourceBundle.getBundle("text.WinLoss");
 
     public WinView(PageController PC, boolean finalLevel, GameSeqLevelController Controller) throws ReadSaveException {
         super(PC);
@@ -29,35 +32,28 @@ public class WinView extends ResultStage {
     }
 
     private void standardDisplayText() {
-        Text t = new Text("You win!");
+        Text t = new Text(myResource.getString("StandardWin"));
         t.setId("Welcome");
         this.getChildren().add(t);
     }
 
     private void displayFinalWinText() {
-        Text t = new Text("You won!\nThis was the final level");
+        Text t = new Text(myResource.getString("FinalWin"));
         t.setId("Welcome");
         this.getChildren().add(t);
     }
 
     private void UpdateCoinDisplay() throws ReadSaveException {
-
-        Text s = new Text("You have received "+PrizeCoins+" coins for winning");
+        Text s = new Text(myResource.getString("Received")+PrizeCoins+myResource.getString("Received2"));
         s.setId("centeredText");
-
         this.getChildren().add(s);
-
         myPC.getUser().updateScore(PrizeCoins);
     }
 
     private void addNextLevelOption(){
-
-        Button next = new Button("Next Level");
-        next.setTranslateX(600);
-        next.setTranslateY(600);
-
+        Button next = new Button(myResource.getString("next"));
+        next.setId("next");
         next.setOnMouseClicked(event -> nextLevel());
-
         this.getChildren().add(next);
 
     }
