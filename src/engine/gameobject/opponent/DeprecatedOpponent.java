ebecb6a96ba6eeb5041d49c091719cfda3e23c7f
Pierce Forte <phf7@duke.edu>
2020-04-29 21:04:49 -0400

    Marked Opponent interface deprecated; added two new images for game objects

---
+++ src/engine/gameobject/opponent/DeprecatedOpponent.java
@@ -0,0 +1,11 @@
+package engine.gameobject.opponent;
+
+/**
+ * This interface defines how a unique opponent will operate
+ */
+@Deprecated
+public interface DeprecatedOpponent {
+
+    void assignLogic();
+
+}
